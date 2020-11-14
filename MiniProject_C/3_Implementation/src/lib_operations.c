fe60eac53c390c6b53513bf1dfa74949ca8968f5
stepin104955 <69070462+stepin104955@users.noreply.github.com>
2020-11-14 12:33:34 +0530

    Update and rename library_operations.c to lib_operations.c

---
+++ MiniProject_C/3_Implementation/src/lib_operations.c
@@ -0,0 +1,57 @@
+#include <lib_operations.h>
+
+
+int Addbook(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int Searchbook(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int Displaybook(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+int Author(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int Titlelist(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+int Stock(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int Issue(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int bookret(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+int Addmembr(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int Exit(int operand1, int operand2)
+{
+    if(0 == operand2)
+        return 0;
+    else
+        return (operand1 / operand2)*100 ;
+    
+    
+}
