cbb44d53b9c63adbbd17e4d849391be555d4ed47
stepin104955 <69070462+stepin104955@users.noreply.github.com>
2020-11-14 16:39:22 +0530

    Update test_lib_operations.c

--- MiniProject_C/3_Implementation/test/test_lib_operations.c
+++ MiniProject_C/3_Implementation/test/test_lib_operations.c
@@ -7,16 +7,28 @@
 #define PROJECT_NAME    "Lib"
 
 /* Prototypes for all the test functions */
-void test_Addbook(void);
-void test_Searchbook(void);
-void test_Displaybook(void);
-void test_Author(void);
-void test_Titlelist(void);
-void test_Stock(void);
-void test_Issue(void);
-void test_bookret(void);
-void test_Addmembr(void);
-void test_Exit(void);
+void test_COMMAND(void);
+void test_USER_NAME_AND_PASSWORD_CHECK(void);
+void test_ADMIN_COMMAND(void);
+void test_STUDENT_COMMAND(void);
+void test_ADD_BOOK(void);
+void test_DELETE_BOOK(void);
+void test_EDIT_BOOK(void);
+void test_BORROW_BOOK(void);
+void test_VIEW_BOOK(void);
+void test_SEARCH_BOOK(void);
+void test_RETURN_BOOK(void);
+void test_CHANGE_USER_NAME(void);
+void test_CHANGE_PASSWORD(void);
+void test_CLEAR(void);
+void test_HELP(void);
+void test_MY_PROFILE(void);
+void test_VIEW_PROFILE(void);
+void test_FORGET_USER_NAME(void);
+void test_FORGET_PASSWORD(void);
+void test_ADD_USER(void);
+void test_RESTART(void);
+void test_LOG_OUT(void);
 
 /* Start of the application test */
 int main() {
@@ -29,16 +41,28 @@ int main() {
   
   
   /* Add your test functions in this format for testing*/
-  CU_add_test(suite, "Addbook", test_Addbook);
-  CU_add_test(suite, "Searchbook", test_Searchbook);
-  CU_add_test(suite, "Displaybook", test_Displaybook);
-  CU_add_test(suite, "Author", test_Author);
-  CU_add_test(suite, "Titlelist", test_Titlelist);
-  CU_add_test(suite, "Stock", test_Stock);
-  CU_add_test(suite, "Issue", test_Issue);
-  CU_add_test(suite, "bookret", test_bookret);
-  CU_add_test(suite, "Addmembr", test_Addmembr);
-  CU_add_test(suite, "Exit", test_Exit);
+  CU_add_test(suite, "COMMAND", test_COMMAND);
+  CU_add_test(suite, "USER_NAME_AND_PASSWORD_CHECK", test_USER_NAME_AND_PASSWORD_CHECK);
+  CU_add_test(suite, "ADMIN_COMMAND", test_ADMIN_COMMAND);
+  CU_add_test(suite, "STUDENT_COMMAND", test_STUDENT_COMMAND);
+  CU_add_test(suite, "ADD_BOOK", test_ADD_BOOK);
+  CU_add_test(suite, "DELETE_BOOK", test_DELETE_BOOK);
+  CU_add_test(suite, "EDIT_BOOK", test_EDIT_BOOK);
+  CU_add_test(suite, "BORROW_BOOK", test_BORROW_BOOK);
+  CU_add_test(suite, "VIEW_BOOK", test_VIEW_BOOK);
+  CU_add_test(suite, "SEARCH_BOOK", test_SEARCH_BOOK);
+  CU_add_test(suite, "RETURN_BOOK", test_RETURN_BOOK);
+  CU_add_test(suite, "CHANGE_USER_NAME", test_CHANGE_USER_NAME);
+  CU_add_test(suite, "CHANGE_PASSWORD", test_CHANGE_PASSWORD);
+  CU_add_test(suite, "CLEAR", test_CLEAR);
+  CU_add_test(suite, "HELP", test_HELP);
+  CU_add_test(suite, "MY_PROFILE", test_MY_PROFILE);
+  CU_add_test(suite, "VIEW_PROFILE", test_VIEW_PROFILE);
+  CU_add_test(suite, "FORGET_USER_NAME", test_FORGET_USER_NAME);
+  CU_add_test(suite, "FORGET_PASSWORD", test_FORGET_PASSWORD);
+  CU_add_test(suite, "ADD_USER", test_ADD_USER);
+  CU_add_test(suite, " RESTART", test_ RESTART);
+  CU_add_test(suite, "LOG_OUT", test_LOG_OUT);
 
 /* Note: Do not edit START*/
   /* Setup Test Framework to output the result to Screen */
@@ -54,72 +78,143 @@ int main() {
 }
 
 /* Write all the test functions */ 
-void test_Addbook(void) {
-  CU_ASSERT(30 == Addbook(10, 20));
+void test_COMMAND(void) {
+  CU_ASSERT(30 == COMMAND(10, 20));
   
   /* Dummy fail*/
-  CU_ASSERT(1500 == Addbook(750, 7500));
+  CU_ASSERT(1500 == COMMAND(750, 7500));
 }
 
-void test_Searchbook(void) {
-  CU_ASSERT(3 == Searchbook(0, 3));
+void test_USER_NAME_AND_PASSWORD_CHECK(void) {
+  CU_ASSERT(3 == USER_NAME_AND_PASSWORD_CHECK(0, 3));
   
   /* Dummy fail*/
-  CU_ASSERT(1 == Searchbook(1000, 900));
+  CU_ASSERT(1 == USER_NAME_AND_PASSWORD_CHECK(1000, 900));
 }
 
-void test_Displaybook(void) {
-  CU_ASSERT(7 == Displaybook(10, 3));
+void test_ADMIN_COMMAND(void) {
+  CU_ASSERT(7 == ADMIN_COMMAND(10, 3));
   
   /* Dummy fail*/
-  CU_ASSERT(1 == Displaybook(1000, 900));
+  CU_ASSERT(1 == ADMIN_COMMAND(1000, 900));
 }
 
-void test_Author(void) {
-  CU_ASSERT(27 == Author(20, 7));
+void test_STUDENT_COMMAND(void) {
+  CU_ASSERT(27 == STUDENT_COMMAND(20, 7));
   
   /* Dummy fail*/
-  CU_ASSERT(1025 == Author(100, 2500));
+  CU_ASSERT(1025 == STUDENT_COMMAND(100, 2500));
 }
 
-void test_Titlelist(void) {
-  CU_ASSERT(13 == Titlelist(16, 3));
+void test_ADD_BOOK(void) {
+  CU_ASSERT(13 == ADD_BOOK(16, 3));
   
   /* Dummy fail*/
-  CU_ASSERT(106 == Titlelist(1080, 900));
+  CU_ASSERT(106 == ADD_BOOK(1080, 900));
 }
 
-void test_Stock(void) {
-  CU_ASSERT(88 == Stock(18, 70));
+void test_DELETE_BOOK(void) {
+  CU_ASSERT(88 == DELETE_BOOK(18, 70));
   
   /* Dummy fail*/
-  CU_ASSERT(1458 == Stock(1450, 900));
+  CU_ASSERT(1458 == DELETE_BOOK(1450, 900));
 }
 
-void test_Issue(void) {
-  CU_ASSERT(56 == Issue(23, 33));
+void test_EDIT_BOOK(void) {
+  CU_ASSERT(56 == EDIT_BOOK(23, 33));
   
   /* Dummy fail*/
-  CU_ASSERT(1999 == Issue(1900, 990));
+  CU_ASSERT(1999 == EDIT_BOOK(1900, 990));
 }
 
-void test_bookret(void) {
-  CU_ASSERT(10 == bookret(80, 70));
+void test_BORROW_BOOK(void) {
+  CU_ASSERT(10 == BORROW_BOOK(80, 70));
   
   /* Dummy fail*/
-  CU_ASSERT(1458 == bookret(1450, 900));
+  CU_ASSERT(1458 == BORROW_BOOK(1450, 900));
 }
 
-void test_Addmembr(void) {
-  CU_ASSERT(100 == Addmembr(80, 20));
+void test_VIEW_BOOK(void) {
+  CU_ASSERT(100 == VIEW_BOOKr(80, 20));
   
   /* Dummy fail*/
-  CU_ASSERT(166 == Addmembr(1386, 800));
+  CU_ASSERT(166 == VIEW_BOOK(1386, 800));
 }
 
-void test_Exit(void) {
-  CU_ASSERT(75 == Exit(30, 40));
+void test_SEARCH_BOOK(void) {
+  CU_ASSERT(75 == SEARCH_BOOK(30, 40));
   
   /* Dummy fail*/
-  CU_ASSERT(200 == Exit(400, 100));
+  CU_ASSERT(200 == SEARCH_BOOK(400, 100));
 }
+void test_RETURN_BOOK(void) {
+  CU_ASSERT(75 == RETURN_BOOK(30, 40));
+  
+  /* Dummy fail*/
+  CU_ASSERT(200 == RETURN_BOOK(400, 100));
+}
+void test_CHANGE_USER_NAME(void) {
+  CU_ASSERT(75 == CHANGE_USER_NAME(30, 40));
+  
+  /* Dummy fail*/
+  CU_ASSERT(200 == CHANGE_USER_NAME(400, 100));
+}
+void test_CHANGE_PASSWORD(void) {
+  CU_ASSERT(75 == CHANGE_PASSWORD(30, 40));
+  
+  /* Dummy fail*/
+  CU_ASSERT(200 == CHANGE_PASSWORD(400, 100));
+}
+void test_CLEAR(void) {
+  CU_ASSERT(75 == CLEAR(30, 40));
+  
+  /* Dummy fail*/
+  CU_ASSERT(200 == CLEAR(400, 100));
+}
+void test_HELP(void) {
+  CU_ASSERT(75 == HELP(30, 40));
+  
+  /* Dummy fail*/
+  CU_ASSERT(200 == HELP(400, 100));
+}
+void test_MY_PROFILE(void) {
+  CU_ASSERT(75 == MY_PROFILE(30, 40));
+  
+  /* Dummy fail*/
+  CU_ASSERT(200 == MY_PROFILE(400, 100));
+}
+void test_VIEW_PROFILE(void) {
+  CU_ASSERT(75 == VIEW_PROFILE(30, 40));
+  
+  /* Dummy fail*/
+  CU_ASSERT(200 == VIEW_PROFILE(400, 100));
+}
+void test_FORGET_USER_NAME(void) {
+  CU_ASSERT(75 == FORGET_USER_NAME(30, 40));
+  
+  /* Dummy fail*/
+  CU_ASSERT(200 == FORGET_USER_NAME(400, 100));
+}
+void test_ ADD_USER(void) {
+  CU_ASSERT(75 ==  ADD_USER(30, 40));
+  
+  /* Dummy fail*/
+  CU_ASSERT(200 == ADD_USER(400, 100));
+}
+void test_ RESTART(void) {
+  CU_ASSERT(75 ==  RESTART(30, 40));
+  
+  /* Dummy fail*/
+  CU_ASSERT(200 ==  RESTART(400, 100));
+}
+void test_  LOG_OUT(void) {
+  CU_ASSERT(75 ==   LOG_OUT(30, 40));
+  
+  /* Dummy fail*/
+  CU_ASSERT(200 ==   LOG_OUT(400, 100));
+}
+
+
+
+
+
