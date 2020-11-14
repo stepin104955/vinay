905948d0e09a23889e2f26ebf68b1335193b3be2
stepin104955 <69070462+stepin104955@users.noreply.github.com>
2020-11-14 16:08:01 +0530

    Update lib_operations.c

--- MiniProject_C/3_Implementation/src/lib_operations.c
+++ MiniProject_C/3_Implementation/src/lib_operations.c
@@ -1,57 +1,102 @@
 #include <lib_operations.h>
 
 
-int Addbook0(int operand1, int operand2)
+int COMMAND(int operand1, int operand2)
 {
     return operand1 + operand2;
 }
 
-/*int Searchbook(int operand1, int operand2)
+int USER_NAME_AND_PASSWORD_CHECK(int operand1, int operand2)
 {
     return operand1 + operand2;
 }
 
-int Displaybook(int operand1, int operand2)
+int ADMIN_COMMAND(int operand1, int operand2)
 {
     return operand1 - operand2;
 }
 
-int Author(int operand1, int operand2)
+int STUDENT_COMMAND(int operand1, int operand2)
 {
     return operand1 + operand2;
 }
 
-int Titlelist(int operand1, int operand2)
+int ADD_BOOK(int operand1, int operand2)
 {
     return operand1 - operand2;
 }
 
-int Stock(int operand1, int operand2)
+int DELETE_BOOK(int operand1, int operand2)
 {
     return operand1 + operand2;
 }
 
-int Issue(int operand1, int operand2)
+int EDIT_BOOK(int operand1, int operand2)
 {
     return operand1 + operand2;
 }
 
-int bookret(int operand1, int operand2)
+int BORROW_BOOK(int operand1, int operand2)
 {
     return operand1 - operand2;
 }
 
-int Addmembr(int operand1, int operand2)
+int VIEW_BOOK(int operand1, int operand2)
 {
     return operand1 + operand2;
-}*/
-
-int Exit1(int operand1, int operand2)
+}
+int SEARCH_BOOK(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+int RETURN_BOOK(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+int CHANGE_USER_NAME(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+int CHANGE_PASSWORD(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+int CLEAR(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+int HELP(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+int MY_PROFILE(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+int VIEW_PROFILE(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+int FORGET_USER_NAME(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+int FORGET_PASSWORD(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+int ADD_USER(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+int RESTART(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+int LOG_OUT(int operand1, int operand2)
 {
     if(0 == operand2)
         return 0;
     else
-        return (operand1 / operand2)*100 ;
-    
-    
+        return (operand1 / operand2)*100 ;   
 }
